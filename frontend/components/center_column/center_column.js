5fbbda75264d3603b9a3b2616e18a7152760358e
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-02 13:05:33 -0700

    Use event bubbling to target pretty and json buttons and insert the corresponding html when each is clicked

--- frontend/components/center_column/center_column.js
+++ frontend/components/center_column/center_column.js
@@ -1,1 +1,26 @@
 import "./center_column.pcss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const centerColumn = document.getElementById("center-col");
+  const forecastContainer = document.getElementById("forecast-container");
+
+  centerColumn.addEventListener("click", event => {
+    if (event.target.classList.contains("format-bttn")) {
+      switch (event.target.id) {
+        case "forecastHTML":
+          forecastContainer.innerHTML = localStorage.getItem("forecastHTML");
+          break;
+        case "forecastJSON":
+          forecastContainer.innerHTML = `
+          <div class="request-format-bttns">
+            <button id="forecastHTML" class="format-bttn">Pretty</button>
+            <button id="forecastJSON" class="format-bttn">JSON</button>
+          </div>
+          <p class="json">${localStorage.getItem("forecastJSON")}</p>
+          `;
+          break;
+        default:
+      }
+    }
+  });
+});
